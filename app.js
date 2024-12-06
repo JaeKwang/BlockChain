4e7b6f1ba8e06cf783db5b120670a587544ab18e
jkpark125 <jkpark125@gmail.com>
2024-12-06 17:22:33 +0900

    가계부 어플

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
-import {API_KEY} from "./config.js";
+const form = document.getElementById("transaction-form");
+const inputName = document.getElementById("transaction-name");
+const inputAmount = document.getElementById("transaction-amount");
+const listContainer = document.getElementById("transaction-list");
+const balanceDisplay = document.getElementById("total-balance");
 
-const endPoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-const newsList = document.getElementById("news-list");
+let transactions = JSON.parse(localStorage.getItem("transaction")) || [];
 
-async function fetchData()
-{
-    try
-    {
-        const res = await fetch(endPoint);
-        const data = await res.json();
-        appendNews(data.articles);
+function addTransaction(event) {
+  event.preventDefault();
 
-    } catch (error){
-        newsList.innerHTML = `<li>Fail to load news => ${error}</li>`;
-    }
+  const name = inputName.value.trim();
+  const amount = parseInt(inputAmount.value.trim());
+
+  if (!name || isNaN(amount)) {
+    alert("이름 혹은 수량을 잘못 입력했습니다.");
+    return;
+  }
+
+  const transaction = {
+    id: Date.now(),
+    name,
+    amount,
+  };
+
+  transactions.push(transaction);
+  updateUI();
+  form.reset();
 }
 
-function appendNews(article)
-{
-    newsList.innerHTML = "";
-    article.forEach((v) => {
-        console.log(v);
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `
-            <h2>${v.title}</h2>
-            ${v.description != null ? `<p>${v.description}</p>` : `<h>No Data</h>`}
-            <a href="${v.url}" target="_blank">link</a>
-        `;
-        newsList.appendChild(listItem);
-    })
+function updateUI() {
+  listContainer.innerHTML = "";
+  let total = 0;
+
+  transactions.forEach((v) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add(v.amount > 0 ? "income" : "expense");
+    listItem.innerHTML = `
+        ${v.name}
+        <span>${v.amount > 0 ? "+" : ""}${v.amount}원</span>
+        <button class="delete-btn" data-id="${v.id}">X</button>
+    `;
+    listContainer.appendChild(listItem);
+
+    // total = total + v.amount;
+    total += v.amount;
+  });
+
+  balanceDisplay.textContent = `${total > 0 ? "+" : ""}${total}원`;
+
+  document.querySelectorAll(".delete-btn").forEach((v) => {
+    v.addEventListener("click", () => deleteTransaction(v.dataset.id));
+  });
+
+  localStorage.setItem("transaction", JSON.stringify(transactions));
 }
 
-fetchData();
+function deleteTransaction(id) {
+  transactions = transactions.filter((v) => v.id !== parseInt(id));
+  updateUI();
+}
+
+form.addEventListener("submit", addTransaction);
+
+updateUI();
